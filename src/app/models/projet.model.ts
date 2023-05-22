70084888b612972a2c19e71e63c189983036abd4
Axel <alopes@btslivh.eu>
2023-05-22 22:03:22 +0200

    clean
    Signed-off-by: Axel <alopes@btslivh.eu>

--- src/app/models/projet.model.ts
+++
@@ -1,9 +0,0 @@
-export class Projet {
-    title?: string;
-    location?: string;
-    description?: string;
-    date?: string;
-    imageUrl?: string;
-    link?: string;
-    linkIMG?: string;
-}
